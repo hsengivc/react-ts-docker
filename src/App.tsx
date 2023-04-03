9a1a8f2d6f69fe4d363c852c83f579aa6beacd76
hsengivc <hsengivc@proton.me>
2023-04-03 16:45:51 +0530

    cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Dockerised</h1>
+      <h1>React + Typescript + Vite + Docker</h1>
     </div>
   );
 }
